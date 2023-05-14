ca04679039c0182c774e3a7d769ee04e2b95fd69
Lars Riel <larsriel@gmail.com>
2023-05-14 16:53:01 +0200

    updated csp

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import "dotenv/config";
 
-const PUBLIC_API_URL = 'https://jazzymcjazz.dk';
+const PUBLIC_API_URL = process.env.PUBLIC_API_URL || 'https://jazzymcjazz.dk';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,7 +20,7 @@ const config = {
 				'script-src': ['self'],
 				'style-src': ['self'],
 				'connect-src': ['self', PUBLIC_API_URL],
-				'img-src': ['self', 'data:', PUBLIC_API_URL],
+				'img-src': ['self', PUBLIC_API_URL],
 				'frame-ancestors': ['none'],
 				'base-uri': ['none'],
 				'form-action': ['self', PUBLIC_API_URL],
